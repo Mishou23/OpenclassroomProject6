20ea7216bc1b31085c97418d93b11d7f17eb19b0
Reymg22 <117723925+Reymg22@users.noreply.github.com>
2023-06-27 13:33:58 -0400

    images added

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,3 +1,15 @@
-const data= await fetch('http://localhost:5678/api/works');
+const data = await fetch('http://localhost:5678/api/works');
 const fullData = await data.json();
-console.log(fullData)
+const gallery = document.querySelector('.gallery');
+
+for (let i = 0; i < fullData.length; i++) {
+  const image = document.createElement("img");
+  const title = document.createElement("p");
+  image.src = fullData[i].imageUrl;
+  title.textContent = fullData[i].title;
+
+  gallery.appendChild(image);
+  title.classList.remove('gallery')
+}
+
+console.log(fullData);
