1dd0d2a02d7c7554671c0901e97e14f43b4b5ee5
Reymg22 <117723925+Reymg22@users.noreply.github.com>
2023-06-28 09:24:05 -0400

    Gallery photos and titles added

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -3,13 +3,16 @@ const fullData = await data.json();
 const gallery = document.querySelector('.gallery');
 
 for (let i = 0; i < fullData.length; i++) {
+  const imageContainer = document.createElement("div"); // Créez un conteneur pour l'image et le titre
   const image = document.createElement("img");
   const title = document.createElement("p");
   image.src = fullData[i].imageUrl;
   title.textContent = fullData[i].title;
 
-  gallery.appendChild(image);
-  title.classList.remove('gallery')
+  imageContainer.appendChild(image); // Ajoutez l'image au conteneur
+  imageContainer.appendChild(title); // Ajoutez le titre au conteneur
+  gallery.appendChild(imageContainer); // Ajoutez le conteneur à la galerie
 }
 
 console.log(fullData);
+
