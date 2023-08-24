6ebffbcef403c6544141807dd52d904bca2d8422
Reymg22 <117723925+Reymg22@users.noreply.github.com>
2023-08-24 01:17:52 -0400

    Final Updates

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -129,12 +129,14 @@ if (token) {
   const galleryPhoto = document.querySelector('.galleryPhoto');
   const background = document.querySelector('.contentContainer');
   const logout = document.querySelector('.logout');
+  const removeSpace = document.querySelector('.contentContainer');
   logout.innerHTML = 'logout';
   logout.addEventListener('click', () => {
     // Remove the token from localStorage
     localStorage.removeItem('token');
     token = null;
   });
+removeSpace.classList.remove('add')
   photoEdit.classList.remove('hidden');
   navbar.classList.remove('hidden');
   galleryEdit.classList.remove('hidden');
@@ -147,13 +149,15 @@ if (token) {
   
   galleryPhoto.style.setProperty('--my-variable', 'your-value');
 const projetFade = document.querySelector('.galleryEdit');
+const navSpace = document.querySelector('.contentContainer');
 
 editGallery.addEventListener('click', handleEditGallery);
 
 function handleEditGallery() {
 
-    galleryPhoto.innerHTML = '<div class="galleryWindow"><div class="exit"><div class="exitContainer"><div class="title"><h2>Galerie photo</h2></div> <div class="return"><a href="./index.html"><i class="fa-solid fa-xmark" style="color: #000000;"></i></a></div></div><div class="allPhotos"></div><div class="galleryEdited"><button class="btnEdit">Ajouter une photo</button><div class="deleteGallery"><p>Supprimer la galerie</p></div></div></div></div>';
+    galleryPhoto.innerHTML = '<div class="galleryWindow"><div class="exit"><div class="exitContainer"><div class="title"><h2>Galerie photo</h2></div> <div class="return"><a href="./index.html"><i class="fa-solid fa-xmark" style="color: #000000;"></i></a></div></div><div class="allPhotos"></div><div class="moveIcon"><i class="fa-solid fa-arrows-up-down-left-right" style="color: #ffffff;"></i></div><div class="galleryEdited"><button class="btnEdit">Ajouter une photo</button><div class="deleteGallery"><p>Supprimer la galerie</p></div></div></div></div>';
     projetFade.classList.add('fade');
+    navSpace.classList.add('show');
     galleryPhoto.classList.add('update');
     background.classList.add('update');
     const allPhotosContainer = document.querySelector('.allPhotos');
@@ -267,7 +271,7 @@ function handleEditGallery() {
     document.addEventListener('mousedown', handleOutsideClick);
 
     //----------------------------------------------------------------------Add Photo to Gallery--------------------------------------------------------------------------------//
-    const editGalleryPhoto = document.querySelector(".btnEdit");
+        const editGalleryPhoto = document.querySelector(".btnEdit");
     editGalleryPhoto.addEventListener('click', () => {
       const galleryPhoto = document.querySelector(".galleryPhoto");
       galleryPhoto.innerHTML = `
@@ -286,24 +290,35 @@ function handleEditGallery() {
           </div>
           <div class="dropZone">
             <div class="imageContainer"></div>
+            <div class="imageContent">
             <div class="imageIcon">
-              <i class="fa-thin fa-image-landscape" style="color: #6f7276;"></i>
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+  <path d="M448 80c8.8 0 16 7.2 16 16V415.8l-5-6.5-136-176c-4.5-5.9-11.6-9.3-19-9.3s-14.4 3.4-19 9.3L202 340.7l-30.5-42.7C167 291.7 159.8 288 152 288s-15 3.7-19.5 10.1l-80 112L48 416.3l0-.3V96c0-8.8 7.2-16 16-16H448zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm80 192a48 48 0 1 0 0-96 48 48 0 1 0 0 96z" fill="rgba(185, 197, 204, 1)"/>
+</svg>
             </div>
-            <span class="selectPhoto">+ Ajouter Photo</span>
+            <div class="photoBtn">
+            <span class="selectPhoto">+ Ajout photo</span>
+            </div>
           </div>
+          <div class="photoDetail">
+          <p>jpg, png : 4mo max</p>
+          </div>
+       
+            </div>
           <form>
             <div class="photoInputs">
               <div class="photoTitle">
-                <h3>Title</h3>
+                <h3>Titre</h3>
                 <input type="text" class="title" id="photoTitle" name="title">
               </div>
 <div class="photoCategory">
-                <h3>Category</h3>
+                <h3>Catégorie</h3>
                 <div class="dropdown">
                   <select class="categoryOptions">
                     <option value="1">Objects</option>
                     <option value="2">Apartments</option>
                     <option value="3">Hotels & Restaurants</option>
+                    <option value="3">Bar & Restaurants</option>
                   </select>
                 </div>
               </div>
@@ -315,6 +330,7 @@ function handleEditGallery() {
         </div>
         </div>
       `
+
 
       const backBtn = document.querySelector('.arrow')
       backBtn.addEventListener('click', (event) => {
